7eadaa2592318ee218817788be57e478c1957100
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-06-16 12:11:55 +0700

    tungnt

--- wp-content/themes/storefront-child/functions/acf.php
+++ wp-content/themes/storefront-child/functions/acf.php
@@ -142,5 +142,115 @@ if( function_exists('acf_add_local_field_group') ):
     'description' => '',
     'show_in_rest' => 0,
   ));
+  acf_add_local_field_group(array(
+    'key' => 'group_62aab5628ec8a',
+    'title' => 'Post Suggest',
+    'fields' => array(
+      array(
+        'key' => 'field_62aab562a9c3c',
+        'label' => 'List Post Suggest',
+        'name' => 'list_post_suggest',
+        'type' => 'relationship',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'post_type' => array(
+          0 => 'post',
+        ),
+        'taxonomy' => '',
+        'filters' => array(
+          0 => 'search',
+          1 => 'post_type',
+          2 => 'taxonomy',
+        ),
+        'elements' => array(
+          0 => 'featured_image',
+        ),
+        'min' => '',
+        'max' => '',
+        'return_format' => 'id',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'post',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => array(
+      0 => 'send-trackbacks',
+    ),
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+  ));
 
+  acf_add_local_field_group(array(
+    'key' => 'group_62aab345dbca7',
+    'title' => 'Product Suggest',
+    'fields' => array(
+      array(
+        'key' => 'field_62aab372741a1',
+        'label' => 'List Product Suggest',
+        'name' => 'list_product_suggest',
+        'type' => 'relationship',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'post_type' => array(
+          0 => 'product',
+        ),
+        'taxonomy' => '',
+        'filters' => array(
+          0 => 'search',
+          1 => 'post_type',
+          2 => 'taxonomy',
+        ),
+        'elements' => array(
+          0 => 'featured_image',
+        ),
+        'min' => '',
+        'max' => '',
+        'return_format' => 'id',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'product',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => array(
+      0 => 'send-trackbacks',
+    ),
+    'active' => true,
+    'description' => '',
+    'show_in_rest' => 0,
+  ));
   endif;
