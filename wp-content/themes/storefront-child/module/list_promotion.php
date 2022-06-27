637c42962f88087d81e5d753316dcf05c8925f0a
tungnt <nguyenthanhtung74119@gmail.com>
2022-06-27 07:14:45 +0700

    tungnt

--- wp-content/themes/storefront-child/module/list_promotion.php
+++ wp-content/themes/storefront-child/module/list_promotion.php
@@ -1,25 +1,25 @@
 <div class="c-sure">
     <ul class="slick-sure">
         <li>
-        <p>Freeship nội thành cho đơn <strong>từ 250.000đ</strong>– Tỉnh <strong>từ 400.000đ</strong></p>
+        <p><?php _e('Freeship within the city for orders <strong>from 250,000 VND</strong>– Province <strong>from 400.000 VND</strong>', 'storefront') ?></p>
         <picture>
             <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.png" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.png" alt="Freeship nội thành cho đơn từ 250.000đ – Tỉnh từ 400.000đ" loading="lazy" width="161" height="134">
         </picture>
         </li>
         <li>
-        <p>Khám thị lực và tư vấn <strong>miễn phí </strong>(đặt lịch trước)</p>
+        <p><?php _e('Vision test and consultation <strong>free </strong>(book in advance)', 'storefront') ?></p>
         <picture>
             <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/eye_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/eye_icon.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/eye_icon.jpg" alt="Khám thị lực và tư vấn miễn phí (đặt lịch trước)" loading="lazy" width="161" height="134">
         </picture>
         </li>
         <li>
-        <p>Thanh toán <strong>quẹt thẻ tận nơi </strong>(áp dụng nội thành HN, TP.HCM)</p>
+        <p><?php _e('Pay <strong>swipe the card on the spot </strong>(applied within Hanoi and HCMC)', 'storefront') ?></p>
         <picture>
             <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.jpg" alt="Thanh toán quẹt thẻ tận nơi (áp dụng nội thành HN, TP.HCM)" loading="lazy" width="161" height="134">
         </picture>
         </li>
         <li>
-        <p>Chế độ bảo <strong>hành vàng cho </strong>trường hợp rách, xước, đồ cộm  </p>
+        <p><?php _e('Protection mode <strong>gold for </strong>case of tear, scratch', 'storefront') ?></p>
         <picture>
             <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.jpg" alt="Chế độ bảo hành vàng cho trường hợp rách, xước, đồ cộm" loading="lazy" width="161" height="134">
         </picture>
