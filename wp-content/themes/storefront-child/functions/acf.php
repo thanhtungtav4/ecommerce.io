e4a833031fa71fd5f1fc9fa7030bc13c3210bf3d
tungnt <nguyenthanhtung74119@gmail.com>
2022-10-09 16:51:22 +0700

    tungnt

--- wp-content/themes/storefront-child/functions/acf.php
+++ wp-content/themes/storefront-child/functions/acf.php
@@ -23,6 +23,59 @@ if (function_exists('acf_add_local_field_group')):
             'parent_slug' => 'theme-general-settings',
         ));
     }
+    acf_add_local_field_group(array(
+        'key' => 'group_63429592ab849',
+        'title' => 'Sản Phẩm mua kèm',
+        'fields' => array(
+            array(
+                'key' => 'field_634295a5efecc',
+                'label' => 'list product together',
+                'name' => 'list_product_together',
+                'type' => 'relationship',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'post_type' => array(
+                    0 => 'product',
+                ),
+                'taxonomy' => '',
+                'filters' => array(
+                    0 => 'search',
+                    1 => 'post_type',
+                    2 => 'taxonomy',
+                ),
+                'elements' => array(
+                    0 => 'featured_image',
+                ),
+                'min' => '',
+                'max' => '',
+                'return_format' => 'id',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'product',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
 
     acf_add_local_field_group(array(
         'key' => 'group_62a6ef8cb1bec',
--- wp-content/themes/storefront-child/module/detail_list_product_suggest.php
+++ wp-content/themes/storefront-child/module/detail_list_product_suggest.php
@@ -1,6 +1,6 @@
 <div class="m-product">
   <div class="m-product_top">
-    <h4>SẢN PHẨM TƯƠNG TỰ</h4>
+    <h4><?php _e('Related products', 'storefront') ?></h4>
     <div class="m-product__nav">
       <button class="m-product__prev">
         <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -24,6 +24,56 @@
             'post_type'   => 'product',
             'post_status' => 'publish',
             'post__in' => get_field('list_product_suggest'),
+          );
+        }
+        else{
+          $args = array(
+            'post_type'   => 'product',
+            'post_status' => 'publish',
+            'posts_per_page' => 5,
+          );
+        }
+        $the_query = new WP_Query($args);
+        if($the_query->have_posts()):
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+        $image = get_the_post_thumbnail_url(get_the_ID(), array(307, 307), array( 'class' => 'lazyload' ));
+      ?>
+        <?php require( get_stylesheet_directory() . '/module/product_item_loop.php' ); ?>
+      <?php 
+        endwhile;
+        endif;
+        // Reset Post Data
+        wp_reset_postdata();
+      ?> 
+    </ul>
+  </div>
+</div>
+<div class="m-product">
+  <div class="m-product_top">
+    <h4><?php _e('Buy together products', 'storefront') ?></h4>
+    <div class="m-product__nav">
+      <button class="m-product__prev">
+        <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+          <path d="M28.835 14.8699L27.065 13.0999L17.165 22.9999L27.065 32.8999L28.835 31.1299L20.705 22.9999L28.835 14.8699H28.835Z"></path>
+        </svg>
+      </button>
+      <button class="m-product__next">
+        <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+          <path d="M18.165 31.1301L19.935 32.9001L29.835 23.0001L19.935 13.1001L18.165 14.8701L26.295 23.0001L18.165 31.1301V31.1301Z" fill="#2B2929"></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+  <div class="m-product__inner w-100">
+    <ul class="m-item w-100">
+      <?php
+        if(get_field('list_product_together')){
+          $args = array(
+            'post_type'   => 'product',
+            'post_status' => 'publish',
+            'post__in' => get_field('list_product_together'),
           );
         }
         else{
