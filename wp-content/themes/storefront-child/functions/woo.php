4046647b3371d1f0c8a562838ac31cfe3aac8fde
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-10-25 17:05:09 +0700

    tungnt

--- wp-content/themes/storefront-child/functions/woo.php
+++ wp-content/themes/storefront-child/functions/woo.php
@@ -74,3 +74,24 @@ add_action('woocommerce_pagination_tungnt', 'add_woocommerce_pagination');
 function add_woocommerce_pagination(){
    woocommerce_pagination();
 }
+if(!is_product()){
+   function wc_varb_price_range( $wcv_price, $product ) {
+
+      $prefix = sprintf('%s: ', __('From', 'wcvp_range'));
+
+      $wcv_reg_min_price = $product->get_variation_regular_price( 'min', true );
+      $wcv_min_sale_price    = $product->get_variation_sale_price( 'min', true );
+      $wcv_max_price = $product->get_variation_price( 'max', true );
+      $wcv_min_price = $product->get_variation_price( 'min', true );
+
+      $wcv_price = ( $wcv_min_sale_price == $wcv_reg_min_price ) ?
+         wc_price( $wcv_reg_min_price ) :
+         '<del>' . wc_price( $wcv_reg_min_price ) . '</del>' . '<ins>' . wc_price( $wcv_min_sale_price ) . '</ins>';
+
+      return ( $wcv_min_price == $wcv_max_price ) ?
+         $wcv_price :
+         sprintf('%s%s', $prefix, $wcv_price);
+   }
+   add_filter( 'woocommerce_variable_sale_price_html', 'wc_varb_price_range', 10, 2 );
+   add_filter( 'woocommerce_variable_price_html', 'wc_varb_price_range', 10, 2 );
+}
