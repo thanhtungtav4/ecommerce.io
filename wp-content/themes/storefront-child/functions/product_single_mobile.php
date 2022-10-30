b741e1ec7d4e4e8b0ff200027a6b80468f79f322
tungnt <nguyenthanhtung74119@gmail.com>
2022-10-30 11:55:48 +0700

    tunntt

--- wp-content/themes/storefront-child/functions/product_single_mobile.php
+++ wp-content/themes/storefront-child/functions/product_single_mobile.php
@@ -24,3 +24,10 @@
     function wc_custome_rating(){
         woocommerce_template_single_rating();
     }
+
+
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
