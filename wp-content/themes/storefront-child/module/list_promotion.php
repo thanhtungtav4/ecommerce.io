efe136a5f81cc38fd0a3d5ad234aa3255a1d17c0
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-11-02 09:43:48 +0700

    scroll behavior

--- wp-content/themes/storefront-child/module/list_promotion.php
+++ wp-content/themes/storefront-child/module/list_promotion.php
@@ -1,6 +1,6 @@
 <div class="c-sure">
     <ul class="slick-sure">
-        <li onclick="window.open('https://caraslens.com/chinh-sach-giao-nhan/')">
+        <li class="is_pointer" onclick="window.open('https://caraslens.com/chinh-sach-giao-nhan/')">
             <p><?php _e('Freeship within the city for orders <strong>from 250,000 VND</strong>– Province <strong>from 400.000 VND</strong>', 'storefront') ?></p>
             <picture>
                 <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.png" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/delivery_icon.png" alt="Freeship nội thành cho đơn từ 250.000đ – Tỉnh từ 400.000đ" loading="lazy" width="161" height="134">
@@ -18,7 +18,7 @@
                 <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/credit_icon.jpg" alt="Thanh toán quẹt thẻ tận nơi (áp dụng nội thành HN, TP.HCM)" loading="lazy" width="161" height="134">
             </picture>
         </li>
-        <li onclick="window.open('https://caraslens.com/chinh-sach-bao-hanh/')">
+        <li class="is_pointer" onclick="window.open('https://caraslens.com/chinh-sach-bao-hanh/')">
             <p><?php _e('Protection mode <strong>gold for </strong>case of tear, scratch', 'storefront') ?></p>
             <picture>
                 <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.svg" type="image/svg"><img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/thumb_icon.jpg" alt="Chế độ bảo hành vàng cho trường hợp rách, xước, đồ cộm" loading="lazy" width="161" height="134">
