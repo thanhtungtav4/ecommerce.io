ed49b2b99687015d03ae82efc551801d662d248a
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-11-25 09:04:11 +0700

    tungnt

---
+++ wp-content/themes/storefront-child/page-cach-deo-len.php
@@ -0,0 +1,309 @@
+<?php
+   /**
+    * The template for displaying all pages.
+    *
+    * This is the template that displays all pages by default.
+    * Please note that this is the WordPress construct of pages
+    * and that other 'pages' on your WordPress site will use a
+    * different template.
+    *
+    * @package storefront
+    */
+   get_header(); ?>
+<main class="l-main">
+   <div class="m-docs">
+      <div class="m-docs_top">
+         <h1 class="m-ttl">THƯ VIỆN KIẾN THỨC KÍNH ÁP TRÒNG - CARAS.E</h1>
+      </div>
+      <div class="l-container">
+         <nav class="m-docs_nav">
+            <a href="#kien-thuc-co-ban-ve-contact-lens">Kiến thức cơ bản về contact lens</a>
+            <a href="#chuan-bi-truoc-khi-mua-lens">Chuẩn bị trước khi mua lens</a>
+            <a href="#kien-thuc-cho-nguoi-moi-deo-lens">Kiến thức cho người mới đeo lens</a>
+            <a href="#kien-thuc-cho-nguoi-deo-lens-lau-nam">Kiến thức cho người đeo lens lâu năm</a>
+            <a href="#life-and-contact-lens">Life & Contact Lens</a>
+         </nav>
+         <div class="m-product" id="kien-thuc-co-ban-ve-contact-lens">
+            <div class="m-product_top">
+               <h4>Kiến thức cơ bản về contact lens</h4>
+               <div class="m-product__nav">
+                  <button class="m-product__prev m-new__prev">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M28.835 14.8699L27.065 13.0999L17.165 22.9999L27.065 32.8999L28.835 31.1299L20.705 22.9999L28.835 14.8699H28.835Z"></path>
+                     </svg>
+                  </button>
+                  <button class="m-product__next m-new__next">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M18.165 31.1301L19.935 32.9001L29.835 23.0001L19.935 13.1001L18.165 14.8701L26.295 23.0001L18.165 31.1301V31.1301Z" fill="#2B2929"></path>
+                     </svg>
+                  </button>
+               </div>
+            </div>
+            <ul class="m-new__slick w-100">
+               <li>
+                  <a href>
+                     <div class="m-new__img">
+                        <picture>
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.avif" type="image/avif">
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.webp" type="image/webp">
+                           <img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" alt="item new" loading="lazy" width="437" height="278">
+                        </picture>
+                     </div>
+                     <div class="m-new__content">
+                        <h3 class="strong">Hướng dẫn các bước skincare từ cơ bản tới nâng cao</h3>
+                        <p class="m-date"><i class="gg-calendar-dates"></i>Tháng Sáu 18, 2021</p>
+                        <p>Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và...</p>
+                     </div>
+                  </a>
+               </li>
+            </ul>
+         </div>
+         <a class="m-btn" href="#">Xem Toàn bộ</a>
+         <div class="m-product" id="chuan-bi-truoc-khi-mua-lens">
+            <div class="m-product_top">
+               <h4>Đọc gì trước khi mua lens?</h4>
+               <div class="m-product__nav">
+                  <button class="m-product__prev m-new__prev">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M28.835 14.8699L27.065 13.0999L17.165 22.9999L27.065 32.8999L28.835 31.1299L20.705 22.9999L28.835 14.8699H28.835Z"></path>
+                     </svg>
+                  </button>
+                  <button class="m-product__next m-new__next">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M18.165 31.1301L19.935 32.9001L29.835 23.0001L19.935 13.1001L18.165 14.8701L26.295 23.0001L18.165 31.1301V31.1301Z" fill="#2B2929"></path>
+                     </svg>
+                  </button>
+               </div>
+            </div>
+            <ul class="m-new__slick w-100">
+               <li>
+                  <a href>
+                     <div class="m-new__img">
+                        <picture>
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.avif" type="image/avif">
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.webp" type="image/webp">
+                           <img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" alt="item new" loading="lazy" width="437" height="278">
+                        </picture>
+                     </div>
+                     <div class="m-new__content">
+                        <h3 class="strong">Hướng dẫn các bước skincare từ cơ bản tới nâng cao</h3>
+                        <p class="m-date"><i class="gg-calendar-dates"></i>Tháng Sáu 18, 2021</p>
+                        <p>Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và...</p>
+                     </div>
+                  </a>
+               </li>
+            </ul>
+         </div>
+         <a class="m-btn" href="#">Xem Toàn bộ</a>
+         <div class="m-docs_post" id="kien-thuc-cho-nguoi-moi-deo-lens">
+            <h2>Kiến thức cho người mới đeo lens</h2>
+            <div class="m-docs_inner">
+               <h3 class="m-ttl3">Ngày 1</h3>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ            </p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ               </p>
+                  </div>
+               </div>
+            </div>
+            <div class="m-docs_inner">
+               <h3 class="m-ttl3">Ngày 2</h3>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ            </p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ                             </p>
+                  </div>
+               </div>
+            </div>
+            <div class="m-docs_inner">
+               <h3 class="m-ttl3">Ngày 1</h3>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ            </p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ               </p>
+                  </div>
+               </div>
+            </div>
+            <div class="m-docs_inner">
+               <h3 class="m-ttl3">Ngày 2</h3>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ            </p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ</p>
+                  </div>
+               </div>
+               <div class="m-docs_item">
+                  <a href="#">Tư vấn các vấn đề về thẻ tín dụng, vay tín chấp các ngân hàng</a>
+                  <div class="m-docs_info">
+                     <a href="#">Marketing </a>
+                     <p>14 MIN READ       </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="m-product" id="kien-thuc-cho-nguoi-deo-lens-lau-nam">
+            <div class="m-product_top">
+               <h4>Kiến thức cho người đeo lens lâu năm</h4>
+               <div class="m-product__nav">
+                  <button class="m-product__prev m-new__prev">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M28.835 14.8699L27.065 13.0999L17.165 22.9999L27.065 32.8999L28.835 31.1299L20.705 22.9999L28.835 14.8699H28.835Z"></path>
+                     </svg>
+                  </button>
+                  <button class="m-product__next m-new__next">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M18.165 31.1301L19.935 32.9001L29.835 23.0001L19.935 13.1001L18.165 14.8701L26.295 23.0001L18.165 31.1301V31.1301Z" fill="#2B2929"></path>
+                     </svg>
+                  </button>
+               </div>
+            </div>
+            <ul class="m-new__slick w-100">
+               <li>
+                  <a href>
+                     <div class="m-new__img">
+                        <picture>
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.avif" type="image/avif">
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.webp" type="image/webp">
+                           <img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" alt="item new" loading="lazy" width="437" height="278">
+                        </picture>
+                     </div>
+                     <div class="m-new__content">
+                        <h3 class="strong">Hướng dẫn các bước skincare từ cơ bản tới nâng cao</h3>
+                        <p class="m-date"><i class="gg-calendar-dates"></i>Tháng Sáu 18, 2021</p>
+                        <p>Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và...</p>
+                     </div>
+                  </a>
+               </li>
+            </ul>
+         </div>
+         <a class="m-btn" href="#">Xem Toàn bộ</a>
+         <div class="m-product" id="life-and-contact-lens">
+            <div class="m-product_top">
+               <h4>Life & Contact Lens</h4>
+               <div class="m-product__nav">
+                  <button class="m-product__prev m-new__prev">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M28.835 14.8699L27.065 13.0999L17.165 22.9999L27.065 32.8999L28.835 31.1299L20.705 22.9999L28.835 14.8699H28.835Z"></path>
+                     </svg>
+                  </button>
+                  <button class="m-product__next m-new__next">
+                     <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="23" cy="23" r="22" stroke-width="2"></circle>
+                        <path d="M18.165 31.1301L19.935 32.9001L29.835 23.0001L19.935 13.1001L18.165 14.8701L26.295 23.0001L18.165 31.1301V31.1301Z" fill="#2B2929"></path>
+                     </svg>
+                  </button>
+               </div>
+            </div>
+            <ul class="m-new__slick w-100">
+               <li>
+                  <a href>
+                     <div class="m-new__img">
+                        <picture>
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.avif" type="image/avif">
+                           <source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.webp" type="image/webp">
+                           <img class="lazyload" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/item-new.jpg" alt="item new" loading="lazy" width="437" height="278">
+                        </picture>
+                     </div>
+                     <div class="m-new__content">
+                        <h3 class="strong">Hướng dẫn các bước skincare từ cơ bản tới nâng cao</h3>
+                        <p class="m-date"><i class="gg-calendar-dates"></i>Tháng Sáu 18, 2021</p>
+                        <p>Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và Đối với các tín đồ làm đẹp, chắc hẳn thuật ngữ skincare đã dần trở nên quen thuộc và...</p>
+                     </div>
+                  </a>
+               </li>
+            </ul>
+         </div>
+         <a class="m-btn" href="#">Xem Toàn bộ</a>
+      </div>
+   </div>
+</main>
+<?php
+get_footer();
