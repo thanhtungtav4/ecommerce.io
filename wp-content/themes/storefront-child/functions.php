3bbee2db82244ca8552a8c3d6778bf8f336088e4
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2023-04-04 14:18:08 +0700

    tungnt

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -1,5 +1,6 @@
 <?php
 require_once( get_stylesheet_directory() . '/functions/global.php' );
+require_once( get_stylesheet_directory() . '/functions/security.php');
 require_once( get_stylesheet_directory() . '/functions/lang.php' );
 require_once( get_stylesheet_directory() . '/functions/optimize.php' );
 require_once( get_stylesheet_directory() . '/functions/resource.php' );
