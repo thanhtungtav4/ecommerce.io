2b394e82319a9dbc5477223ea7f8dee289e45e9e
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2023-04-26 09:34:01 +0700

    tunnt

---
+++ wp-content/themes/storefront-child/page-sales-promotion.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+get_header(); ?>
+ <main class="l-main">
+  <div class="l-container col-2 flexcenter flex-wrap promotion-container">
+    <dl>
+      <dd>
+        <a href="http://caraslens.com/sales-promotion/">
+        <picture>
+          <source media="(max-width:768px)" srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/mctkm-30-pearlsapphire.jpg" type="image/jpeg">
+          <img loading="lazy" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-30-pearlsapphire.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-30-pearlsapphire.jpg" alt="ctkm-30-pearlsapphire" width="326" height="149">
+        </picture>
+        </a>
+      </dd>
+      </dt>
+    </dl>
+    <dl>
+      <dd>
+        <a href="http://caraslens.com/sales-promotion/">
+        <picture>
+          <source media="(max-width:768px)" srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/mctkm-combolens1day.jpg" type="image/jpeg">
+          <img loading="lazy" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-combolens1day.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-combolens1day.jpg" alt="ctkm-combolens1day" width="326" height="149">
+        </picture>
+        </a>
+      </dd>
+    </dl>
+    <dl>
+      <dd>
+        <a href="http://caraslens.com/sales-promotion/">
+        <picture>
+          <source media="(max-width:768px)" srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/mctkm-dd-vitamin.jpg" type="image/jpeg">
+          <img loading="lazy" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-dd-vitamin.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-dd-vitamin.jpg" alt="ctkm-dd-vitamin" width="326" height="149">
+        </picture>
+        </a>
+      </dd>
+    </dl>
+    <dl>
+      <dd>
+        <a href="http://caraslens.com/sales-promotion/">
+        <picture>
+          <source media="(max-width:768px)" srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/mctkm-10-5.jpg" type="image/jpeg">
+          <img loading="lazy" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-10-5.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-10-5.jpg" alt="ctkm-10-5" width="326" height="149">
+        </picture>
+        </a>
+      </dd>
+    </dl>
+    <dl>
+      <dd>
+        <a href="http://caraslens.com/sales-promotion/">
+        <picture>
+          <source media="(max-width:768px)" srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/mctkm-10danhgia.jpg" type="image/jpeg">
+          <img loading="lazy" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-10danhgia.jpg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ctkm-10danhgia.jpg" alt="ctkm-10danhgia" width="326" height="149">
+        </picture>
+        </a>
+      </dd>
+    </dl>
+  </div>
+ </main>
+<?php
+get_footer();
