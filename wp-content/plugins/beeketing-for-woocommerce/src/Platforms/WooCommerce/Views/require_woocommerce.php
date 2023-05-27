8e57211235788e2fe7772a747c0a30ee58277ef2
tungnt <nguyenthanhtung74119@gmail.com>
2023-05-27 14:49:43 +0700

    tungnt

--- wp-content/plugins/beeketing-for-woocommerce/src/Platforms/WooCommerce/Views/require_woocommerce.php
+++
@@ -1,4 +0,0 @@
-<div class="notice notice-warning notice-alt beeketing-warning is-dismissible">
-    <p>Beeketing plugin requires <a href="<?php _e($wooCommercePluginUrl); ?>" target="_blank">WooCommerce</a> to work because it helps to convert visitors into subscribers and sales for WooCommerce online stores.
-    <p><a class="button button-primary" href="<?php _e($wooCommercePluginUrl); ?>" target="_blank">View WooCommerce</a></p></p>
-</div>
